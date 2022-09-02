c697acbe0726a05fd5e00846f6d6c8d2bf215980
Magdalena Pastuła <pastula.magdalena@gmail.com>
2022-09-02 19:31:40 +0200

    Adjusted python script to nucleo source.

--- python_src/python_script.py
+++ python_src/python_script.py
@@ -1,16 +1,38 @@
 import serial
 from serial.tools import list_ports
 
+
+def check_response():
+    response = ser.read_until(terminator=b'\n').decode('utf-8')
+
+    if response != "OK\n":
+        print("Did not receive response from board\n")
+        exit(-1)
+
+
 # spi_speed = int(input())
 spi_speed = input()
 
 # TODO: check spi speed?
 
 ports = list(list_ports.grep(r'.*STM.*'))
-ser = serial.Serial(ports[0].device, 115200)    # TODO: baud rate to change?
+ser = serial.Serial(ports[0].device, 9600, timeout=1)
 
-ser.write(bytes(spi_speed, "ascii"))
+# clear serial from any buffered data
+ser.read_until(terminator=b'\n')
 
-read_bytes = ser.read_until(terminator=b';').decode('utf-8')
+ser.write(bytes(spi_speed, "ok."))
+
+check_response()
+
+ser.write(bytes(spi_speed, "freq:" + spi_speed))
+
+check_response()
+
+ser.write(bytes(spi_speed, "id01"))
+
+check_response()
+
+read_bytes = ser.read_until(terminator=b'\n').decode('utf-8')
 
 print("Response:", read_bytes, "\n")
